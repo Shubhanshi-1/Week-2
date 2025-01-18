d004c2e60243aa4427fa0545fc1e3466ef073086
Shubhanshi <vermashashi3421@gmail.com>
2025-01-18 23:54:10 +0530

    Question 3

---
+++ InstanceClassVariableAndMethod/Vehicles.java
@@ -0,0 +1,45 @@
+class Vehicle {
+    
+    private String ownerName;
+    private String vehicleType;
+
+    // Class variable 
+    private static double registrationFee = 100.00;
+
+    // Constructor
+    public Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    // Instance method to display vehicle details
+    public void displayVehicleDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: " + registrationFee);
+    }
+
+    // Class method to update the registration fee
+    public static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+
+    
+}
+
+
+public class Vehicles {
+    // Main method
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle("Alice", "Car");
+        Vehicle v2 = new Vehicle("Bob", "Motorcycle");
+
+        v1.displayVehicleDetails();
+        v2.displayVehicleDetails();
+
+        Vehicle.updateRegistrationFee(120.00);
+
+        v1.displayVehicleDetails();
+        v2.displayVehicleDetails();
+    }
+}
